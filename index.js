17dbd9d813e156ecae1677f5905396cafcb79d72
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-11-16 21:59:15 +0600

    complete

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Shaven assignment 12 !')
+    res.send('Shaven Server running !')
 })
 
 app.listen(port, () => {
